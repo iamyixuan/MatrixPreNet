fixed gradient problem through CG solver

DiracNPC/NPC_CG.py
@@ -31,32 +31,35 @@ trainIdx, valIdx = split_idx(data_exp.shape[0], 42)
 trainData = data_exp[trainIdx]
 valData = data_exp[valIdx]
 
-TrainLoader = create_dataLoader(data=np.array(trainData), batchSize=256, kappa=0.276,shuffle=True)
-ValLoader = create_dataLoader(data=np.array(valData), kappa=0.276, batchSize=256, shuffle=False)
+TrainLoader = create_dataLoader(data=np.array(trainData), batchSize=200, kappa=0.276,shuffle=True)
+ValLoader = create_dataLoader(data=np.array(valData), kappa=0.276, batchSize=128, shuffle=False)
 
 
 epochs = 1000
-learning_rate = 1e-4
-h_ch = 64
+learning_rate = 1e-5
+h_ch = 16
 
-model = Encoder_Decoder(2, 2, h_ch, (3, 3))
+model = Encoder_Decoder(2, 4, h_ch, (3, 3))
 
 key = jax.random.PRNGKey(121)
 state = init_train_state(model, key, (1, 8, 8, 2), learning_rate)
+LOG = True
 
-# run = wandb.init(
-#     # Set the project where this run will be logged
-#     project="reconstructDiracConfig",
-#     name='NN-PCG',
-#     # Track hyperparameters and run metadata
-#     config={
-#         "learning_rate": learning_rate,
-#         "epochs": epochs,
-#         "h_ch": h_ch,
-#     })
-final_state = train_val(TrainLoader, ValLoader, state, epochs, diracOpt=DDOpt, model=model, verbose=True, log=False)
+if LOG:
+    run = wandb.init(
+        # Set the project where this run will be logged
+        project="reconstructDiracConfig",
+        name='NN-LinearOpt-2',
+        # Track hyperparameters and run metadata
+        config={
+            "learning_rate": learning_rate,
+            "epochs": epochs,
+            "h_ch": h_ch,
+        })
+        
+final_state = train_val(TrainLoader, ValLoader, state, epochs, diracOpt=DDOpt, model=model, verbose=False, log=LOG)
 
-# ckpt = {'model': final_state}
-# orbax_checkpointer = orbax.checkpoint.PyTreeCheckpointer()
-# save_args = orbax_utils.save_args_from_target(ckpt)
-# orbax_checkpointer.save('/Users/yixuan.sun/Documents/projects/Preconditioners/MatrixPreNet/checkpoints/NN-PCG/model', ckpt, save_args=save_args)
+ckpt = {'model': final_state}
+orbax_checkpointer = orbax.checkpoint.PyTreeCheckpointer()
+save_args = orbax_utils.save_args_from_target(ckpt)
+orbax_checkpointer.save('/Users/yixuan.sun/Documents/projects/Preconditioners/MatrixPreNet/checkpoints/NN-PCG/model_LinearOpt', ckpt, save_args=save_args)

NeuralPC/model/CNNs_flax.py
@@ -50,7 +50,7 @@ class CNNDecoder(nn.Module):
     def setup(self):
         self.Tconv_1 = nn.ConvTranspose(self.out_ch, self.ker_size, strides=(2, 2))
         self.Tconv_2 = nn.ConvTranspose(self.h_ch, self.ker_size, strides=(2, 2))
-        self.Tconv_3 = nn.ConvTranspose(self.in_ch, self.ker_size, strides=(2, 2))
+        self.Tconv_3 = nn.ConvTranspose(4, self.ker_size, strides=(2, 2))
         self.dense1 = nn.Dense(512)
         self.dense2 = nn.Dense(256)
         self.dense3 = nn.Dense(128)
@@ -143,7 +143,7 @@ class DiracPreconditionerCNN(nn.Module):
         self.conv2 = nn.Conv(features=32, kernel_size=(3, 3), use_bias=True)
         self.conv3 = nn.Conv(features=64, kernel_size=(3, 3), use_bias=True)
         self.dense = nn.Dense(features=self.L**4)
-        self.final_conv = nn.Conv(features=2, kernel_size=(1, 1), use_bias=True)
+        self.final_conv = nn.Conv(features=4, kernel_size=(1, 1), use_bias=True)
 
     @nn.compact
     def __call__(self, x, train: bool):

NeuralPC/train/TrainFlax.py
@@ -4,9 +4,11 @@ import numpy as np
 import optax
 import jax.numpy as jnp
 import wandb
+from ..model.linearOpt import linearConvOpt
 from flax.training import train_state
 from ..utils.losses import PCG_loss
 from functools import partial
+from tqdm import tqdm
 # from ..utils.dirac import DiracOperator
 
 
@@ -60,8 +62,12 @@ def init_train_state(model, random_key, shape, learning_rate) -> train_state.Tra
         batch_stats=variables["batch_stats"],
     )
 
+@jax.jit
+def average_gradient_norm(grads):
+    grads = jax.tree_util.tree_leaves(grads)
+    avg_grad_norm = jnp.mean(jnp.array([jnp.linalg.norm(g) for g in grads]))
+    return avg_grad_norm
 
-#@jax.jit
 def train_step(
     state: train_state.TrainState, 
     batch: jnp.ndarray,  
@@ -79,41 +85,26 @@ def train_step(
     b = random_b(key, shape=in_mat.shape)
 
     U1_field = jnp.transpose(in_mat, axes=(0, 3, 1, 2))
-    lossFunc = partial(
-        PCG_loss, U1=U1_field, b=b, kappa=kappa, steps=1000, operator=diracOpt
-    )
 
-    def NNopt(x):
-        y_precond, _ = model.apply(
-            {"params": state.params, "batch_stats": state.batch_stats}, x, mutable=['batch_stats'], train=True
-        )
-        return y_precond
-    
-    # x = random_b(key, b.shape)
-    # print(x.shape)
-    # y = NNopt(x)
-    # print(y.shape)
-    # assert False
-
-    def loss_fn(params):
-        pred, updates = state.apply_fn(
-            {"params": params, "batch_stats": state.batch_stats},
-            in_mat,
-            train=True,
-            mutable=["batch_stats"],
-        )
-        # loss = jnp.mean((pred - out_mat)**2)
-        loss = lossFunc(NNopt)
-        return loss, updates
-
-    gradient_fn = jax.value_and_grad(loss_fn, has_aux=True)
-    (loss, updates), grads = gradient_fn(state.params)
+    gradient_fn = jax.value_and_grad(PCG_loss, has_aux=True)
+    (loss, updates), grads = jax.jit(gradient_fn, static_argnums=[2, 7, 8, 9])(
+                                state.params,
+                                batch_stats=state.batch_stats,
+                                model=model,
+                                U1=U1_field,
+                                b=b,
+                                in_mat=in_mat,
+                                kappa=kappa,
+                                steps=100,
+                                operator=diracOpt,
+                                train=True
+                            )
     state = state.apply_gradients(grads=grads)
     state = state.replace(batch_stats=updates["batch_stats"])
-    return state, loss, key
+    return state, loss, key, grads
 
 
-#@jax.jit
+
 def eval_step(state, batch,  diracOpt, model, key):
     in_mat, kappa = batch
     kappa = float(kappa[0])
@@ -124,23 +115,19 @@ def eval_step(state, batch,  diracOpt, model, key):
     b = random_b(key, shape=in_mat.shape)
     U1_field = jnp.transpose(in_mat, axes=(0, 3, 1, 2))
 
-    def NNopt(x):
-        y_precond = model.apply(
-            {"params": state.params, "batch_stats": state.batch_stats}, x, mutable=False, train=False
-        )
-        return y_precond
+    loss, updates = PCG_loss(
+                params=state.params,
+                batch_stats=state.batch_stats,
+                model=model,
+                U1=U1_field,
+                b=b,
+                in_mat=in_mat,
+                kappa=kappa,
+                steps=100,
+                operator=diracOpt,
+                train=False
+            )
 
-    lossFunc = partial(
-        PCG_loss, U1=U1_field, b=b, kappa=kappa, steps=1000, operator=diracOpt
-    )
-    loss = lossFunc(NNopt)
-
-    # pred = state.apply_fn(
-    #     {"params": state.params, "batch_stats": state.batch_stats},
-    #     in_mat,
-    #     train=False,
-    #     mutable=False,
-    # )
     return loss, key
 
 
@@ -150,23 +137,27 @@ def train_val(trainLoader, valLoader, state, epochs, diracOpt, model, verbose, l
     for ep in range(epochs):
         # training
         trainBatchLoss = []
-        for train_batch in trainLoader:
+        gradients = []
+        for train_batch in tqdm(trainLoader):
             batch = [train_batch[0].numpy(), train_batch[1].numpy()]
-            state, loss, trainKey = train_step(state=state, batch=batch, diracOpt=diracOpt, model=model, key=trainKey)
+            state, loss, trainKey, grads = train_step(state=state, batch=batch, diracOpt=diracOpt, model=model, key=trainKey)
             trainBatchLoss.append(loss)
+            gradients.append(average_gradient_norm(grads))
+            # break # just for prelim results
         valBatchLoss = []
         for val_batch in valLoader:
             Vbatch = [val_batch[0].numpy(), val_batch[1].numpy()]
             vLoss, valKey = eval_step(state, Vbatch, diracOpt, model, valKey)
             valBatchLoss.append(vLoss)
+            break # only eval one batch
         if verbose == True:
             print(
-                "Epoch {}, train loss {:.4f} validation loss {:.4f}".format(
-                    ep + 1, np.mean(trainBatchLoss), np.mean(valBatchLoss)
+                "Epoch {}, grads norm {:.4f} train loss {:.4f} validation loss {:.4f}".format(
+                    ep + 1, np.mean(gradients), np.mean(trainBatchLoss), np.mean(valBatchLoss)
                 )
             )
         if log:
             wandb.log(
-                {"trainLoss": np.mean(trainBatchLoss), "valLoss": np.mean(valBatchLoss)}
+                {"trainLoss": np.mean(trainBatchLoss), "valLoss": np.mean(valBatchLoss),'grads': np.mean(gradients)}
             )
     return state

NeuralPC/utils/losses.py
@@ -5,6 +5,8 @@ import jax.numpy as jnp
 import jax
 from jax import jit
 from functools import partial
+from ..utils.conjugate_gradient import solve
+from ..model.linearOpt import linearConvOpt
 
 
 class Losses:
@@ -61,28 +63,44 @@ class Losses:
         else:
             raise Exception("Loss name not recognized!")
 
-#@partial(jit, static_argnums=(0, 5))
-def PCG_loss(NN, U1, b, kappa, steps, operator):
+# @partial(jit, static_argnums=(0, 5))
+def PCG_loss(params, 
+             batch_stats,
+             model, 
+             U1, 
+             b, 
+             in_mat,
+             kappa, 
+             steps, 
+             operator,
+             train=False):
     """
-    NN: the neural network, acting as the preconditioning operator.
+    opt: the linear operator that act as the preconditioner 
     U1: gauge configuration of shape (b, 2, L, L).
     steps: total steps for PCG to run.
     operator: the operator of the original system.
     """
-    print(U1.shape)
-    # NN_vmap = jax.vmap(NN) # use this with equinox Module
-    def runPCG(operator, b, precond=NN):
-        x = jnp.ones(b.shape).astype(b.dtype)
-        print( b.dtype, x.dtype)
-        x_sol = cg(A=operator, b=b, x0=x, M=precond, maxiter=steps)
-        return x_sol
+   
+    def runPCG(operator, b):
+        kernels, updates = model.apply(
+         {"params": params, "batch_stats": batch_stats}, in_mat, mutable=['batch_stats'], train=train
+        )
+        kernels = jnp.reshape(kernels, (kernels.shape[:-1] + (2, 2)))
+
+        def PCopt(x):
+            x = x[:, jnp.newaxis, ...]
+            new_vect = jax.vmap(linearConvOpt, in_axes=[0, 0])(x, kernels)
+            return new_vect
+        
+        x_sol = solve(A=operator, b=b, M=PCopt, max_iters=steps)
+        return x_sol, updates
 
     # fix the iteration step, calculate the residual b - Ax_sol and minimize the squared value.
 
     opt = partial(operator, U1=U1, kappa=kappa)
-    x_sol, _ = runPCG(opt, b=b)
-    residual = b - opt(x_sol)
-    return jnp.mean(jnp.abs(residual)**2)
+    state, updates = runPCG(opt, b=b)
+    residual = b - opt(state.x)
+    return jnp.mean(jnp.abs(residual)**2), updates
 
 def testLoss(NN, x):
     NN_v = jax.vmap(NN)